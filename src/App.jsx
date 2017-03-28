0cf5b479c6e10ddcdd26f2f6972863eb90374c20
Daniel <daniel.zamorano.m@gmail.com>
2017-03-28 15:54:38 -0600

    Polished with react bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Clock from './Clock.jsx';
 import './App.css'
+import { Form, FormControl, Button} from 'react-bootstrap';
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
       deadline: 'December 25, 2017',
-      newDeadline: 'December 30, 2017'
+      newDeadline: null
     };
   }
 
@@ -21,9 +22,15 @@ class App extends Component {
         <div className="app-title">
           Countdown to {this.state.deadline}
         </div>
-        <Clock />
-        <input placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})}/>
-        <button onClick={() => this.changeDeadline()}> Submit </button>
+        <Clock deadline={this.state.deadline}/>
+        <Form inline action="#">
+          <FormControl
+            className="deadline-input"
+            placeholder="new date"
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadline()}> Submit </Button>
+        </Form>
       </div>
     )
   }
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -13,13 +13,34 @@ class Clock extends Component {
     };
   }
 
+  componentWillMount () {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount () {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
+  leading0 (num) {
+    return num < 10 ? '0' + num : num;
+  }
+
+  getTimeUntil (deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor((time/(1000*60*60)) % 24);
+    const days = Math.floor(time/(1000*60*60*24));
+    this.setState({days, hours, minutes, seconds});
+  }
+
   render () {
     return (
       <div>
-        <div className="clock-days">0 days</div>
-        <div className="clock-hours">0 hours</div>
-        <div className="clock-minutes">0 minutes</div>
-        <div className="clock-seconds"> seconds</div>
+        <div className="clock-days"> { this.leading0(this.state.days) } days</div>
+        <div className="clock-hours"> { this.leading0(this.state.hours) } hours</div>
+        <div className="clock-minutes"> { this.leading0(this.state.minutes) } minutes</div>
+        <div className="clock-seconds"> { this.leading0(this.state.seconds) } seconds</div>
       </div>
     )
   }
